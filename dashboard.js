0b18f1ddc12dbccdee872760977f3d0fab57c306
jxeon <jamesmawm@gmail.com>
2017-01-15 23:36:26 +0800

    New features:
    - Enable keywords to be added or removed dynamically.
    - Keywords can be saved to external file for reading and loading.

--- dashboard.js
+++ dashboard.js
@@ -6,59 +6,98 @@ var Express = require('express');
 var Http = require('http');
 var Twitter = require('twit');
 var SocketIO = require('socket.io');
+var Fs = require('fs');
 
 var app = Express();
 app.use(Express.static(__dirname + '/public'));
 
 // Routes
-app.get('/', function(req, res, next){
-  res.render('/public/index.html');
+app.get('/', function (req, res, next) {
+	res.render('/public/index.html');
 });
 
 var server = Http.createServer(app);
 server.listen(process.env.LISTEN_PORT, process.env.LISTEN_HOST);
 console.log("Running on http://" + process.env.LISTEN_HOST + ":" + process.env.LISTEN_PORT);
 
-var io  = SocketIO.listen(server);
+var io = SocketIO.listen(server);
 io.set('log level', 0);
 
 var twit = new Twitter({
-    consumer_key:  process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_SECRET
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_SECRET
 });
 
-var track_list = process.env.TRACK_LIST.split(',').map(function (word) {
-    return word.trim();
+var stream = null;
+var track_list = [];
+
+Fs.readFile(process.env.TRACK_FILE_NAME, 'utf8', function (err, data) {
+	if (err) {
+		console.log("Error reading file:", err);
+		return;
+	}
+
+	track_list = data.split(',').map(function (word) {
+		return word.trim();
+	});
+	stream = twit.stream('statuses/filter', {track: track_list});
 });
-var stream = twit.stream('statuses/filter', { track: track_list });
 
-io.sockets.on('connection', function(socket){
-    console.log('io.sockets.on connection');
+function restart_stream() {
+	stream.stop();
+	stream = twit.stream('statuses/filter', {track: track_list});
+}
 
-    socket.on('data', function(action,data){
-        if(action==='*') {
-            twit.updateStatus(data, function (err, data){
+function save_config(text) {
+	Fs.writeFile(process.env.TRACK_FILE_NAME, text, function (err) {
+		if (err)
+			return console.log(err);
+	});
+}
 
-            });
-        }
-    });
+io.sockets.on('connection', function (socket) {
+	console.log('io.sockets.on connection');
 
-    socket.on('getfilter', function() {
-        socket.emit('pushfilter', track_list);
-    });
+	socket.on('data', function (action, data) {
+		if (action === '*') {
+			twit.updateStatus(data, function (err, data) {
+
+			});
+		}
+	});
+
+	socket.on('getfilter', function () {
+		socket.emit('pushfilter', track_list);
+	});
+
+	socket.on('addKeyword', function (keyword) {
+		track_list.push(keyword);
+		restart_stream();
+		save_config(track_list.join(","));
+	});
+
+	socket.on('removeKeyword', function (keyword) {
+		var index = track_list.indexOf(keyword);
+		if (index == -1)
+			return; // nothing to remove
+
+		track_list.splice(index, 1);
+		restart_stream();
+		save_config(track_list.join(","));
+	});
 
 
-    stream.on('tweet', function(tweet) {
-        socket.emit('message', JSON.stringify(tweet));
-    });
+	stream.on('tweet', function (tweet) {
+		socket.emit('message', JSON.stringify(tweet));
+	});
 
-    stream.on('error', function(err){
-        console.log("Err: " + err);
-    });
+	stream.on('error', function (err) {
+		console.log("Err: " + err);
+	});
 
-    socket.on('disconnect', function() {
-      console.log('Disconnect.');
-  });
+	socket.on('disconnect', function () {
+		console.log('Disconnect.');
+	});
 });
--- public/myscript.js
+++ public/myscript.js
@@ -1,84 +1,88 @@
-var filters='';
+var filters = '';
 var socket = io.connect();
 
-socket.on("connect", function() {
-    console.log("connected");
-    socket.emit('getfilter', function() {
-    });
+socket.on("connect", function () {
+	console.log("connected");
+	socket.emit('getfilter', function () {
+	});
 });
 
-socket.on("disconnect", function() {
-    console.log("disconnected");
-    socket.disconnect();
+socket.on("disconnect", function () {
+	console.log("disconnected");
+	socket.disconnect();
 });
 
-socket.on('pushfilter', function(f) {
-    filters = f;
-    $('#tracker').empty();
-    filters.forEach(function(str){
-//        var node =  $('<div class="alert alert-info" id="'+str+'"><a class="close" data-dismiss="alert" id="'+str+'" href="#">&times;</a><p>'+str+'</p></div></div>')
-        var node =  $('<li id="'+str+'"><a href="#" class="">'+str+'</a></li>');
-        node.prependTo("#tracker");
-    });
+socket.on('pushfilter', function (f) {
+	filters = f;
+	$('#tracker').empty();
+	filters.forEach(function (keyword) {
+		addKeywordToList(keyword);
+	});
 });
 
-socket.on('message', function(json) {
-    data = JSON.parse(json);
-    var replacePattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    var replacedText = (data.text).replace(replacePattern, '<a href="$1" target="_blank">$1</a>');
+socket.on('message', function (json) {
+	var data = JSON.parse(json);
+	var replacePattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+	var replacedText = (data.text).replace(replacePattern, '<a href="$1" target="_blank">$1</a>');
 
-    filters.forEach(function(str) {
-        var search = new RegExp(str, "gim");
-        replacedText = replacedText.replace(search, '<span class="label label-danger">'+str+'</span>');
-    });
+	filters.forEach(function (str) {
+		var search = new RegExp(str, "gim");
+		replacedText = replacedText.replace(search, '<span class="label label-danger">' + str + '</span>');
+	});
 
-    var html = "<span class='display_text' onclick='display_snapshot(this);' >[" + data.user.screen_name + "] " + replacedText + "</span>";
-    html += " &nbsp;&nbsp;<span><a href='#' onclick='show_data(this);'>data</a><div style='display:none' class='panel data'>" + json + "</div></span>";
-    var node = $("<li ></li>").html(html);
-    node.prependTo("ul.unstyled");//.css({opacity:0}).slideDown("slow").animate({opacity:1},"slow");
+	var html = "<span class='display_text' onclick='display_snapshot(this);' >[" + data.user.screen_name + "] " + replacedText + "</span>";
+	html += "&nbsp;&nbsp;<span><a href='#' onclick='show_data(this);'>data</a><div style='display:none' class='panel data'>" + json + "</div></span>";
+	var node = $("<li ></li>").html(html);
+	node.prependTo("ul.unstyled");//.css({opacity:0}).slideDown("slow").animate({opacity:1},"slow");
 });
 
-function display_snapshot(e){
-    var html  = $(e).html();
-    var data = $(e).parent().find('.data').text();
-    html += "&nbsp;&nbsp;<a href='#' onclick='show_data(this);'>data</a>";
-    html += "<div style='display:none;' class='panel'>" + data + "</div>";
-    $('#snapbox_box').html(html);
+function display_snapshot(e) {
+	var html = $(e).html();
+	var data = $(e).parent().find('.data').text();
+	html += "&nbsp;&nbsp;<a href='#' onclick='show_data(this);'>data</a>";
+	html += "<div style='display:none;' class='panel'>" + data + "</div>";
+	$('#snapbox_box').html(html);
 }
 
 /*
-Show the raw json data
-*/
-function show_data(e){
-    var target_display = $(e).parent().find('div');
-    if (target_display.is(':visible')){
-        target_display.hide();
-    }
-    else{
-        target_display.show();
-    }
-    return false;
+ Show the raw json data
+ */
+function show_data(e) {
+	var target_display = $(e).parent().find('div');
+	if (target_display.is(':visible')) {
+		target_display.hide();
+	}
+	else {
+		target_display.show();
+	}
+	return false;
 }
 
-
-function addTrack() {
-    socket.emit( 'data', '+', $("#data").val());
-    $("#data").val('');
-    socket.disconnect();
-    socket.socket.reconnect();
+function addKeywordToList(keyword) {
+	var node = $('<div id="' + keyword + '" class="tracker"><b><a href="#" class="tracker_link">&times;</a></b>&nbsp;&nbsp;<span class="keyword">' + keyword + '</span></div>');
+	node.prependTo("#tracker");
 }
 
-$("#tracker").delegate('a', 'click', function() {
-    socket.emit( 'data', '-', $(this).attr('id'));
-    socket.disconnect();
-    socket.socket.reconnect();
-});
+function addKeyword() {
+	var input = $('#input_tracker');
+	var keyword = input.val();
 
-function tweetThat() {
-    socket.emit( 'data', '*', $('#tweet').val());
-    $('#tweet').val('');
+	socket.emit('addKeyword', keyword);
+	addKeywordToList(keyword);
+
+	socket.disconnect();
+	socket.socket.reconnect();
 }
 
-$(document).ready(function(){
-
+$(document).ready(function () {
+	// Removing a keyword from the list
+	$(document).on('click', '.tracker', function (_) {
+		var keyword = $(this).find('.keyword').text();
+		socket.emit('removeKeyword', keyword);
+		$(this).remove();
+		//window.location.reload();
+		socket.disconnect();
+		socket.socket.reconnect();
+		return false;
+	});
 });
